e9c5fe821703d2d54553f88ccd1f5c07facd05b9
Sam Saccone <samccone@gmail.com>
2017-07-02 11:40:47 -0700

    k

--- index.ts
+++ index.ts
@@ -1,15 +1,35 @@
-import { processSourceMaps } from "./process";
+import { processSourceMaps } from './process';
+import * as meow from 'meow';
 
-const files = process.argv.slice(2);
-const processed = processSourceMaps(files);
+const cli = meow(
+	`
+  Usage:
+    bundle-buddy  <source_map_glob>
 
-console.log(
-  JSON.stringify({
-    graph: processed.graph,
-    sourceFiles: processed.sourceFiles,
-    bundleFileStats: [...processed.bundleFileStats],
-    outputFiles: processed.outputFiles,
-    groupedBundleStats: [...processed.groupedBundleStats],
-    stats: [...processed.stats]
-  })
+  Options:
+    --stdout -o: Write analysis to stdout
+  
+  Example:
+    bundle-buddy my_app/dist/*.map
+`,
+	{
+		alias: {
+			o: 'stdout'
+		}
+	}
 );
+
+const processed = processSourceMaps(cli.input);
+
+if (cli.flags['stdout']) {
+	console.log(
+		JSON.stringify({
+			graph: processed.graph,
+			sourceFiles: processed.sourceFiles,
+			bundleFileStats: [ ...processed.bundleFileStats ],
+			outputFiles: processed.outputFiles,
+			groupedBundleStats: [ ...processed.groupedBundleStats ],
+			stats: [ ...processed.stats ]
+		})
+	);
+}
