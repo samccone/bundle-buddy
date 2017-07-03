7fabce4a736c96f6f6dd99a183f84dd2a81007b6
Sam Saccone <samccone@gmail.com>
2017-07-03 15:11:51 -0700

    Fit contents of file viewer

--- viz/src/BottomPanel.js
+++ viz/src/BottomPanel.js
@@ -1,65 +1,69 @@
-import React, { Component } from "react";
-import { select } from "d3-selection";
-import { scaleLinear, scaleQuantize } from "d3-scale";
-import { colorScale } from "./color";
+import React, { Component } from 'react';
+import { select } from 'd3-selection';
+import { scaleLinear, scaleQuantize } from 'd3-scale';
+import { colorScale } from './color';
 
-const width = 400;
+const width = 200;
 const height = 500;
 
 function drawFile({ outputFile, updateSelectedSource }) {
-  const svg = select("svg#fileMap");
+	const svg = select('svg#fileMap');
 
-  if (outputFile) {
-    let totalCount = 0;
-    const files = Object.keys(outputFile[1])
-      .map(d => {
-        return {
-          name: d,
-          ...outputFile[1][d]
-        };
-      })
-      .filter(d => d.inBundleCount > 1)
-      .sort((a, b) => b.inBundleCount - a.inBundleCount)
-      .map(d => {
-        d.totalCount = totalCount;
-        totalCount += d.count + 5;
-        return d;
-      });
+	if (outputFile) {
+		let totalCount = 0;
+		const files = Object.keys(outputFile[1])
+			.map((d) => {
+				return {
+					name: d,
+					...outputFile[1][d]
+				};
+			})
+			.filter((d) => d.inBundleCount > 1)
+			.sort((a, b) => b.inBundleCount - a.inBundleCount)
+			.map((d) => {
+				d.totalCount = totalCount;
+				totalCount += d.count + 5;
+				return d;
+			});
 
-    const chunks = svg.select("g.chunks").selectAll("rect").data(files);
+		const chunks = svg.select('g.chunks').selectAll('rect').data(files);
 
-    const yScale = scaleLinear().domain([0, totalCount]).range([0, height]);
-    chunks
-      .enter()
-      .append("rect")
-      .on("click", d => updateSelectedSource(d.name))
-      .merge(chunks)
-      .attr("width", 200)
-      .attr("y", d => yScale(d.totalCount))
-      .attr("fill", d => colorScale(d.inBundleCount))
-      .attr("height", d => yScale(d.count));
-  }
+		const yScale = scaleLinear().domain([ 0, totalCount ]).range([ 0, height ]);
+		chunks
+			.enter()
+			.append('rect')
+			.on('click', (d) => updateSelectedSource(d.name))
+			.merge(chunks)
+			.attr('width', 200)
+			.attr('y', (d) => yScale(d.totalCount))
+			.attr('fill', (d) => colorScale(d.inBundleCount))
+			.attr('height', (d) => yScale(d.count));
+	}
 }
 
 class BottomPanel extends Component {
-  componentDidMount() {
-    drawFile(this.props);
-  }
+	componentDidMount() {
+		drawFile(this.props);
+	}
 
-  render() {
-    const { summarySentence, sourceView } = this.props;
+	render() {
+		const { summarySentence, sourceView } = this.props;
 
-    return (
-      <div className="col-xs-12">
-        {summarySentence}
-        <svg id="fileMap" width={width} height={height}>
-          <g className="chunks" />
-          <g className="annotations" />
-        </svg>
-        {sourceView}
-      </div>
-    );
-  }
+		return (
+			<div className="col-xs-12">
+				{summarySentence}
+				<div className="source-details">
+					<svg id="fileMap" width={width} height={height}>
+						<g className="chunks" />
+						<g className="annotations" />
+					</svg>
+          <div className="source-container">
+					{sourceView}
+          </div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default BottomPanel;
