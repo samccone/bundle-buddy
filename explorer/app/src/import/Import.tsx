c4807627b94c62a0e38f9b305230d41864546a02
Sam Saccone <samccone@gmail.com>
2018-12-22 20:54:22 -0800

    Implement initial browser based stats.json input
    - file reading
    - graph generation
    - graph cleaning
    - deduping logic

--- explorer/app/src/import/Import.tsx
+++ explorer/app/src/import/Import.tsx
@@ -1,20 +1,41 @@
 import React, { Component } from "react";
+import { cleanGraph } from './graph_process';
+import { statsToGraph } from "./stats_to_graph";
 
 // noopener noreferrer
 
 class Import extends Component {
+  sourceMapInput?: React.RefObject<HTMLInputElement & { files: FileList }>;
+  statsInput?: React.RefObject<HTMLInputElement & { files: FileList }>;
+
   constructor(props: {}) {
     super(props);
 
+    this.sourceMapInput = React.createRef();
+    this.statsInput = React.createRef();
     this.changeSelected = this.changeSelected.bind(this);
   }
 
-  state: {selected: boolean|null} = {
+  state: { selected: boolean | null } = {
     selected: null
   };
 
   changeSelected(selected: boolean) {
     this.setState({ selected });
+  }
+
+  processFiles() {
+    if (this.statsInput != null && this.statsInput.current != null) {
+      const statsFile = this.statsInput.current.files[0];
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        const target = (e.target as EventTarget & { result: string });
+        const stats = JSON.parse(target.result);
+        console.log(cleanGraph(statsToGraph(stats)));
+      }
+
+      reader.readAsText(statsFile);
+    }
   }
 
   render() {
@@ -23,6 +44,12 @@ class Import extends Component {
         <header className="App-header">
           <h1>Bundle Buddy</h1>
         </header>
+
+        <label htmlFor="stats" >stats.json</label>
+        <input id="stats" type="file" ref={this.statsInput}></input>
+        <label htmlFor="sourcemap">sourcemap</label>
+        <input id="sourcemap" type="file" ref={this.sourceMapInput}></input>
+        <button onClick={() => this.processFiles()}>Process</button>
       </div>
     );
   }
---
+++ explorer/app/src/import/prefix_cleaner.ts
@@ -0,0 +1,30 @@
+export function findCommonPrefix(strings: string[]) {
+    let commonPrefix = null;
+
+    for (const k of strings) {
+        if (commonPrefix == null) {
+            commonPrefix = k;
+        } else {
+            let matched = false;
+            for (let splitPoint = k.length; splitPoint > 0; splitPoint--) {
+                const split = k.slice(0, splitPoint);
+                if (commonPrefix.indexOf(split) != -1) {
+                    commonPrefix = split;
+                    matched = true;
+                    break;
+                }
+
+                if (split.length === 1) {
+                    break;
+                }
+            }
+
+            if (!matched) {
+                commonPrefix = '';
+                break;
+            }
+        }
+    }
+
+    return commonPrefix;
+}
